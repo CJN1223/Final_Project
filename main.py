0458a93fe27638816f01b4d0b4d637299ebc356a
Connor Nohe <cjn12.ps4@gmail.com>
2026-04-28 20:48:29 -0400

    Replay feature

--- main.py
+++ main.py
@@ -2,40 +2,48 @@ import random
 
 print("Number Guessing Game")
 print("----------------------------------------------------")
-print("Choose your difficulty level:")
-print("1. Easy (1-100)")
-print("2. Medium (1-500)")
-print("3. Hard (1-1000)")
 
-difficulty = input("Enter Easy, Medium, or Hard: ")
-while difficulty not in ["Easy", "Medium", "Hard"]:
-    print("Invalid input. Please enter Easy, Medium, or Hard.")
+play_again = "yes"
+
+while play_again.lower() == "yes":
+    print("Choose your difficulty level:")
+    print("1. Easy (1-100)")
+    print("2. Medium (1-500)")
+    print("3. Hard (1-1000)")
+
     difficulty = input("Enter Easy, Medium, or Hard: ")
+    while difficulty not in ["Easy", "Medium", "Hard"]:
+        print("Invalid input. Please enter Easy, Medium, or Hard.")
+        difficulty = input("Enter Easy, Medium, or Hard: ")
 
-if difficulty == "Easy":
-    max_num = 100
-elif difficulty == "Medium":
-    max_num = 500
-else:
-    max_num = 1000
+    if difficulty == "Easy":
+        max_num = 100
+    elif difficulty == "Medium":
+        max_num = 500
+    else:
+        max_num = 1000
 
-num = random.randint(1, max_num)
+    num = random.randint(1, max_num)
 
-attempts = 0
-correctGuess = False
+    attempts = 0
+    correctGuess = False
 
-while not correctGuess:
-    try:
-        guess = int(input(f"Enter your guess (1-{max_num}): "))
-        attempts += 1
+    while not correctGuess:
+        try:
+            guess = int(input(f"Enter your guess (1-{max_num}): "))
+            attempts += 1
 
-        if guess > num:
-            print("Too high! Try again.")
-        elif guess < num:
-            print("Too low! Try again.")
-        else:
-            print("Correct! You won the game!")
-            print("Attempts: ", attempts)
-            correctGuess = True
-    except ValueError:
-        print("Invalid input. Please enter a number between 1 and 100.")
+            if guess > num:
+                print("Too high! Try again.")
+            elif guess < num:
+                print("Too low! Try again.")
+            else:
+                print("Correct! You won the game!")
+                print("Attempts: ", attempts)
+                correctGuess = True
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 100.")
+    
+    play_again = input("Do you want to play again? (Yes/No): ")
+    while play_again.lower() not in ["yes", "No"]:
+        play_again = input("Invalid input. Please enter Yes or No: ")
