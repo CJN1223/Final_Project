3441e8c3e1dc70b2f4a4829de91c827f103e030c
Connor Nohe <cjn12.ps4@gmail.com>
2026-04-28 20:18:35 -0400

    Base done

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Project Started')
+import random
+
+print("Number Guessing Game")
+print("----------------------------------------------------")
+
+num = random.randint(1, 100)
+
+attempts = 0
+correctGuess = False
+
+while not correctGuess:
+    try:
+        guess = int(input("Enter your guess (1-100): "))
+        attempts += 1
+
+        if guess > num:
+            print("Too high! Try again.")
+        elif guess < num:
+            print("Too low! Try again.")
+        else:
+            print("Correct! You won the game!")
+            print("Attempts: ", attempts)
+            correctGuess = True
+    except ValueError:
+        print("Invalid input. Please enter a number between 1 and 100.")
